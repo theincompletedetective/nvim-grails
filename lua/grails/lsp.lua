345de6c5afe81223990a61744fc4815f01a87c6c
Elijah Smith <esmith1412@posteo.net>
2025-06-07 13:15:45 -0400

    To use Neovim 0.11+ config options

--- lua/grails/lsp.lua
+++ lua/grails/lsp.lua
@@ -26,15 +26,17 @@ function M.setup()
 
   local home = vim.fn.expand("~")
 
-  lspconfig.groovyls.setup({
+  vim.lsp.config("groovyls", {
     cmd = { home .. "/.local/share/nvim/mason/bin/groovy-language-server" },
     root_dir = lspconfig.util.root_pattern("grails-app"),
-    filetypes = { "groovy" },
+    filetypes = { "groovy", "gsp" },
     settings = {},
     on_attach = function(client, bufnr)
       -- Optional: buffer local mappings or diagnostic config
     end,
   })
+
+  vim.lsp.enable("groovyls")
 end
 
 return M
