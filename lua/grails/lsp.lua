3a5191b0bfd5a90f2fba10fd808a04486194affd
Elijah Smith <esmith1412@posteo.net>
2025-06-07 13:22:15 -0400

    To display unsupported feature

--- lua/grails/lsp.lua
+++ lua/grails/lsp.lua
@@ -10,6 +10,7 @@ function M.setup()
   end
 
   local ok_mason, mason_registry = pcall(require, "mason-registry")
+
   if not ok_mason then
     return
   end
@@ -20,9 +21,21 @@ function M.setup()
   end
 
   local ok_lspconfig, lspconfig = pcall(require, "lspconfig")
+
   if not ok_lspconfig then
     return
   end
+
+  vim.lsp.config("groovyls", {
+    root_dir = lspconfig.util.root_pattern("grails-app"),
+    filetypes = { "groovy", "gsp" },
+    settings = {},
+    on_attach = function(client, bufnr)
+      -- Optional: buffer local mappings or diagnostic config
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.documentRangeFormattingProvider = false
+    end,
+  })
 end
 
 return M
