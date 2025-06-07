5f71522f173629e4fac57f3659e84a96cd01940f
Elijah Smith <esmith1412@posteo.net>
2025-06-07 08:43:51 -0400

    To fix bug

--- lua/nvim-grails/config.lua
+++ lua/nvim-grails/config.lua
@@ -1,51 +1,53 @@
 local M = {}
 
+local home = vim.fn.expand("~")
+
 local default_config = {
-	lsp = {
-		cmd = {
-			"java",
-			"-jar",
-			"~/.local/share/groovy/groovy-language-server-all.jar",
-		},
-		filetypes = { "groovy", "gsp" },
-		root_dir = function(fname)
-			local util = require("lspconfig.util")
-			return util.root_pattern("build.gradle", "grails-app", "gradle.properties", "settings.gradle")(fname)
-		end,
-		settings = {
-			groovy = {
-				classpath = {},
-				gradle = {
-					enabled = true,
-					wrapper = { enabled = true },
-				},
-				grails = {
-					enabled = true,
-					autoDetect = true,
-				},
-				imports = {
-					aliases = {
-						["grails.gorm"] = "org.grails.datastore.gorm",
-						["grails.web"] = "org.grails.web",
-					},
-				},
-			},
-		},
-	},
-	gsp = {
-		enable_syntax = true,
-		enable_ftplugin = true,
-	},
-	commands = {
-		grails_executable = "./gradlew grails-run-command --args=",
-	},
-	scaffold = {
-		templates_dir = vim.fn.stdpath("config") .. "/grails-templates",
-	},
+  lsp = {
+    cmd = {
+      "java",
+      "-jar",
+      home .. "/.local/share/groovy/groovy-language-server-all.jar",
+    },
+    filetypes = { "groovy", "gsp" },
+    root_dir = function(fname)
+      local util = require("lspconfig.util")
+      return util.root_pattern("build.gradle", "grails-app", "gradle.properties", "settings.gradle")(fname)
+    end,
+    settings = {
+      groovy = {
+        classpath = {},
+        gradle = {
+          enabled = true,
+          wrapper = { enabled = true },
+        },
+        grails = {
+          enabled = true,
+          autoDetect = true,
+        },
+        imports = {
+          aliases = {
+            ["grails.gorm"] = "org.grails.datastore.gorm",
+            ["grails.web"] = "org.grails.web",
+          },
+        },
+      },
+    },
+  },
+  gsp = {
+    enable_syntax = true,
+    enable_ftplugin = true,
+  },
+  commands = {
+    grails_executable = "./gradlew grails-run-command --args=",
+  },
+  scaffold = {
+    templates_dir = vim.fn.stdpath("config") .. "/grails-templates",
+  },
 }
 
 function M.setup(opts)
-	M.config = vim.tbl_deep_extend("force", default_config, opts or {})
+  M.config = vim.tbl_deep_extend("force", default_config, opts or {})
 end
 
 return M
